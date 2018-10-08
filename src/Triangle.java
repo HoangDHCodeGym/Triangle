6e8a438208502fba065fc01b75a7947a250a2b85
Hoang Dao <huuhoang.dao@gmail.com>
2018-10-08 11:50:57 +0700

    Initial commit

---
+++ src/Triangle.java
@@ -0,0 +1,64 @@
+public class Triangle extends Shape{
+    private double side1;
+    private double side2;
+    private double side3;
+
+    public Triangle() {
+        super();
+        this.side1 = 1;
+        this.side2 = 1;
+        this.side3 = 1;
+    }
+
+    public Triangle(double side1, double side2, double side3) {
+        super();
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    public Triangle(String color, boolean filled, double side1, double side2, double side3) {
+        super(color, filled);
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    public double getSide1() {
+        return side1;
+    }
+
+    public void setSide1(double side1) {
+        this.side1 = side1;
+    }
+
+    public double getSide2() {
+        return side2;
+    }
+
+    public void setSide2(double side2) {
+        this.side2 = side2;
+    }
+
+    public double getSide3() {
+        return side3;
+    }
+
+    public void setSide3(double side3) {
+        this.side3 = side3;
+    }
+
+    public double getPerimeter() {
+        return side1 + side2 + side3;
+    }
+
+    public double getArea() {
+        double p = this.getPerimeter()/2;
+        return Math.sqrt(p*(p-side1)*(p-side2)*(p-side3));
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Triangle {%.2f, %.2f, %.2f} which is a subclass of ", side1, side2, side3) + super.toString();
+    }
+}
